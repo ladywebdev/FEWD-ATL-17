8a1836b6d7def7908315603db429fb4de8db4881
Brooke Guy <ladywebdev@outlook.com>
2018-01-03 18:05:21 -0500

    Week 4 Homework

---
+++ Week 4/Assignment/brooke_guy_week8_homework/starter_code/script.js
@@ -0,0 +1,46 @@
+// JavaScript File
+
+/*## Technical Requirements 
+
+- Prevent a form submission with the ```event.preventDefault()``` function
+- Use the ```$.ready()``` handler to delay your code from executing until all DOM assets have been loaded
+- Select the appropriate DOM elements with CSS selectors upon a user's click using the ```$.click()``` handler
+- If a user clicks "Read More" on the primary column:
+
+  - have the text in the ```<p>``` tag slide down along with a "Read Less" link in the blog post using  ```$.slideDown()``` and ```$.show()```
+  - hide the "Read More" link using ```$.hide()```
+
+- If a user clicks "Read Less" on the primary column:
+
+  - have the ```<p>``` slide up and hide the "Read Less" link using  ```$.slideUp()``` and ```$.hide()```
+  - show the "Read More" link using ```$.show()```
+
+- Using the same functions as above, if a user clicks the "Learn More" link in the sidebar, have the ```<span>``` inside that ```<p>``` slide down and hide the "Learn More" link using ```$.slideDown()``` and ```$.hide()```
+*/
+
+
+
+$(function() {
+    console.log( "ready!" );
+});
+
+$(document).ready(function(){
+    $(".readmore").click(function(event){
+    	event.preventDefault();
+        $("#show-this-on-click").slideDown();
+     	$(".readmore").hide();
+     	$(".readless").show();
+     })
+    $(".readless").click(function(event){
+    	event.preventDefault();
+        $("#show-this-on-click").slideUp();
+     	$(".readmore").show();
+     	$(".readless").hide();
+     })
+    $(".learnmore").click(function(event){
+    	event.preventDefault();
+        $("#learnmoretext").slideDown();
+     	$("span").show();
+     	$(".readless").hide();
+     })
+});
--- Week 4/lesson 08 - Intro to jQuery/starter_code/traffic_light/js/index.js
+++ Week 4/lesson 08 - Intro to jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,29 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+$(document).ready(function() {
+	alert{'Hello World!'};
+
+	$('#stopButton').click(illuminateRed);
+	$('#slowButton').click(illuminateYellow);
+	$('#goButton').click(illuminate Green);
+
+	function illuminateRed() {
+		clearlights();
+		$('#stopLight').css('background','red');
+	}
+
+	function illuminateYellow() {
+		clearlights();
+		$('#slowLight').css('background','yellow');
+	}
+
+	function illuminateGreen() {
+		clearlights();
+		$('#goLight').css('background','green');
+	}
+
+	function clearLights() {
+		$('#traffic-light div').css('background','black');
+	}
+
+});
